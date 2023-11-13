5c63c94b4152814b3d301f6d71e8785006a9059f
shishir3d <shishirpoudel2061@gmail.com>
2023-11-13 08:47:55 +0545

    made a greet func to say hello to any name

--- django/lecture3/hello/urls.py
+++ django/lecture3/hello/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('shishir', views.shishir, name='shishir'),
-    path('bob', views.bob, name='bob')
+    path('bob', views.bob, name='bob'),
+    path('<str:name>', views.greet, name="greet")
     # other URL patterns
 ]
--- django/lecture3/hello/views.py
+++ django/lecture3/hello/views.py
@@ -7,3 +7,5 @@ def shishir(request):
     return HttpResponse("<h1 style='color:red'>Hello Shishir!</h1>")
 def bob(request):
     return HttpResponse("<h1 style='color:red'>Hello Bob!</h1>")
+def greet(request, name):
+    return HttpResponse(f"<h1 style='color:red'>Hello {name}!</h1>")
