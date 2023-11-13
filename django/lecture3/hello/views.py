184f202f9769260292180690e2d0b08480f0279d
shishir3d <shishirpoudel2061@gmail.com>
2023-11-13 09:02:20 +0545

    made hello page load hello/index.html

--- django/lecture3/hello/views.py
+++ django/lecture3/hello/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1 style='color:red'>Hello!</h1>")
+    return render(request, "hello/index.html")
 def shishir(request):
     return HttpResponse("<h1 style='color:red'>Hello Shishir!</h1>")
 def bob(request):
     return HttpResponse("<h1 style='color:red'>Hello Bob!</h1>")
 def greet(request, name):
-    return HttpResponse(f"<h1 style='color:red'>Hello {name}!</h1>")
+    return HttpResponse(f"<h1 style='color:red'>Hello {name.upper()}!</h1>")
