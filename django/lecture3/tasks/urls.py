d3fbd775c85f5946a1237eaa8f984b527c7511e8
shishir3d <shishirpoudel2061@gmail.com>
2023-11-22 14:16:22 +0545

    modified add.html and index.html

--- django/lecture3/tasks/urls.py
+++ django/lecture3/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "tasks" #app_name is a django keyword
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('add', views.addtask, name = 'addtask')
