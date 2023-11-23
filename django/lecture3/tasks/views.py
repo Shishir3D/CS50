7e296c5c5b28f4addb5c79ec46e7e5e164b4cdd2
shishir3d <shishirpoudel2061@gmail.com>
2023-11-23 14:26:08 +0545

    now adding tasks in the add.html shows up in the index.html

--- django/lecture3/tasks/views.py
+++ django/lecture3/tasks/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django import forms
+from django.urls import reverse
 
-task = ['apple', 'octopus', 'rhino']
+tasks = []
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10 )
 
 def index(request):
     return render(request, 'tasks/index.html', {
-       "tasks" : task 
+       "tasks" : tasks 
     })
 
 def addtask(request):
+    if request.method == "POST":
+        form =  NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, 'tasks/add.html', {
+                "form" : form
+            })
     return render(request, 'tasks/add.html', {
         "form" : NewTaskForm()
     })
