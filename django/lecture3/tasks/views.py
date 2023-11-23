11510b6ec61b196f00b250cc89d2ff42f9fe6a50
shishir3d <shishirpoudel2061@gmail.com>
2023-11-23 16:49:10 +0545

    now two sessions does not have the same tasks in the todo section

--- django/lecture3/tasks/views.py
+++ django/lecture3/tasks/views.py
@@ -3,23 +3,25 @@ from django.http import HttpResponseRedirect
 from django import forms
 from django.urls import reverse
 
-tasks = []
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
     # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10 )
 
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, 'tasks/index.html', {
-       "tasks" : tasks 
+       "tasks" : request.session["tasks"]
     })
 
 def addtask(request):
+
     if request.method == "POST":
         form =  NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, 'tasks/add.html', {
